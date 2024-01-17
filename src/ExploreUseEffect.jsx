9fe0c6ab3bfeba557d00cb69cd0d471a8439424a
Al Hasanul Banna <alhasanulbanna@gmail.com>
2024-01-17 23:45:51 +0600

    update useEffect

--- src/ExploreUseEffect.jsx
+++ src/ExploreUseEffect.jsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from "react";
+// Usecase 1: fetching data from API
+/*import React, { useEffect, useState } from "react";
 
 const ExploreUseEffect = () => {
   const [users, setUsers] = useState([]);
+
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
@@ -32,3 +34,75 @@ function UserEmail(props) {
 }
 
 export default ExploreUseEffect;
+*/
+
+// Usecase 2: Update Timer
+/*import React, { useEffect, useState } from "react";
+
+const ExploreUseEffect = () => {
+  const [time, setTime] = useState(new Date());
+  const [timer, setTimer] = useState(0);
+
+  useEffect(() => {
+    console.log("Starting times");
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTimer((preCount) => preCount + 1);
+    }, 1000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h3>Time: {time.toLocaleTimeString()}</h3>
+      <h3>Timer: {timer} </h3>
+    </div>
+  );
+};
+
+export default ExploreUseEffect;
+*/
+
+// Usecase 3: Update DOM
+/*import React, { useEffect, useState } from "react";
+
+const ExploreUseEffect = () => {
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => {
+    setCount((preCount) => preCount + 1);
+  };
+  const decreseCount = () => {
+    setCount((preCount) => preCount - 1);
+  };
+  const resetCount = () => {
+    setCount(0);
+  };
+
+  useEffect(() => {
+    console.log("Update");
+    document.title = `Click ${count} times`; // it runs every times when rendering
+  }, [count]);
+
+  return (
+    <div>
+      <h4>Count: {count}</h4>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreseCount}>Decrease</button>
+      <button onClick={resetCount}>Reset</button>
+    </div>
+  );
+};
+
+export default ExploreUseEffect;
+*/
